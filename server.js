e9f484d89157ce670ec5e7a96b492669d078c97b
Ivan5 <converse_ivan@hotmail.com>
2019-06-03 17:05:24 -0500

    Make RootQueryType and schema of books

--- server.js
+++ server.js
@@ -1,5 +1,65 @@
 const express = require("express");
-
+const expressGraphQL = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
+} = require("graphql");
 const app = express();
+
+//sample Data
+const authors = [
+  { id: 1, name: "J.K. Rowling" },
+  { id: 2, name: "J.R.R. Tolkien" },
+  { id: 3, name: "Brent Weeks" }
+];
+
+const books = [
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaben", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Followship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyonc the Shadowa", authorId: 3 }
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represent a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) }
+  })
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of Books",
+      resolve: () => books
+    }
+  })
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType
+});
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true
+  })
+);
 
 app.listen(5000, () => console.log("Server started at 5000"));
