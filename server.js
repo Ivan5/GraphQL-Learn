e7ae6608c1cf62243b235ce366cdd9331e15869c
Ivan5 <converse_ivan@hotmail.com>
2019-06-03 17:31:42 -0500

    Add authorType and implements in RootQueryType

--- server.js
+++ server.js
@@ -34,7 +34,28 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
     name: { type: GraphQLNonNull(GraphQLString) },
-    authorId: { type: GraphQLNonNull(GraphQLInt) }
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: book => {
+        return authors.find(author => author.id === book.authorId);
+      }
+    }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represent a author of a book",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: author => {
+        return books.filter(book => book.authorId === author.id);
+      }
+    }
   })
 });
 
@@ -46,6 +67,11 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       description: "List of Books",
       resolve: () => books
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "List of all Authors",
+      resolve: () => authors
     }
   })
 });
